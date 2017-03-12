b0637e7a611fe123506c83358257218e8f6baae3
Arnoldas Sescila <arnoldass@wix.com>
2017-03-12 23:08:09 +0200

    Add sudoku tests under describe

--- test/sudoku.js
+++ test/sudoku.js
@@ -107,46 +107,46 @@ describe('Sudoku', () => {
     expect(validSolution(invalidBoard2)).to.equal(false);
     expect(validSolution(invalidBoard3)).to.equal(false);
   });
-});
 
-describe('columns', () => {
-  it('should return colums', () => {
-    expect(getColumns(validBoard1)).to.deep.equal(
-      [
-        [5, 6, 1, 8, 4, 7, 9, 2, 3],
-        [3, 7, 9, 5, 2, 1, 6, 8, 4],
-        [4, 2, 8, 9, 6, 3, 1, 7, 5],
-        [6, 1, 3, 7, 8, 9, 5, 4, 2],
-        [7, 9, 4, 6, 5, 2, 3, 1, 8],
-        [8, 5, 2, 1, 3, 4, 7, 9, 6],
-        [9, 3, 5, 4, 7, 8, 2, 6, 1],
-        [1, 4, 6, 2, 9, 5, 8, 3, 7],
-        [2, 8, 7, 3, 1, 6, 4, 5, 9]
-      ]);
-  });
-});
-
-describe('blocks', () => {
-  it('should return one block', () => {
-    expect(getBlock(validBoard1, 0, 0)).to.deep.equal([5, 3, 4, 6, 7, 2, 1, 9, 8]);
-    expect(getBlock(validBoard1, 0, 3)).to.deep.equal([6, 7, 8, 1, 9, 5, 3, 4, 2]);
-    expect(getBlock(validBoard1, 0, 6)).to.deep.equal([9, 1, 2, 3, 4, 8, 5, 6, 7]);
-    expect(getBlock(validBoard1, 6, 6)).to.deep.equal([2, 8, 4, 6, 3, 5, 1, 7, 9]);
+  describe('columns', () => {
+    it('should return colums', () => {
+      expect(getColumns(validBoard1)).to.deep.equal(
+        [
+          [5, 6, 1, 8, 4, 7, 9, 2, 3],
+          [3, 7, 9, 5, 2, 1, 6, 8, 4],
+          [4, 2, 8, 9, 6, 3, 1, 7, 5],
+          [6, 1, 3, 7, 8, 9, 5, 4, 2],
+          [7, 9, 4, 6, 5, 2, 3, 1, 8],
+          [8, 5, 2, 1, 3, 4, 7, 9, 6],
+          [9, 3, 5, 4, 7, 8, 2, 6, 1],
+          [1, 4, 6, 2, 9, 5, 8, 3, 7],
+          [2, 8, 7, 3, 1, 6, 4, 5, 9]
+        ]);
+    });
   });
 
-  it('should return blocks', () => {
-    expect(getBlocks(validBoard1)).to.deep.equal(
-      [
-        [5, 3, 4, 6, 7, 2, 1, 9, 8],
-        [6, 7, 8, 1, 9, 5, 3, 4, 2],
-        [9, 1, 2, 3, 4, 8, 5, 6, 7],
-        [8, 5, 9, 4, 2, 6, 7, 1, 3],
-        [7, 6, 1, 8, 5, 3, 9, 2, 4],
-        [4, 2, 3, 7, 9, 1, 8, 5, 6],
-        [9, 6, 1, 2, 8, 7, 3, 4, 5],
-        [5, 3, 7, 4, 1, 9, 2, 8, 6],
-        [2, 8, 4, 6, 3, 5, 1, 7, 9]
-      ]
-    );
+  describe('blocks', () => {
+    it('should return one block', () => {
+      expect(getBlock(validBoard1, 0, 0)).to.deep.equal([5, 3, 4, 6, 7, 2, 1, 9, 8]);
+      expect(getBlock(validBoard1, 0, 3)).to.deep.equal([6, 7, 8, 1, 9, 5, 3, 4, 2]);
+      expect(getBlock(validBoard1, 0, 6)).to.deep.equal([9, 1, 2, 3, 4, 8, 5, 6, 7]);
+      expect(getBlock(validBoard1, 6, 6)).to.deep.equal([2, 8, 4, 6, 3, 5, 1, 7, 9]);
+    });
+
+    it('should return blocks', () => {
+      expect(getBlocks(validBoard1)).to.deep.equal(
+        [
+          [5, 3, 4, 6, 7, 2, 1, 9, 8],
+          [6, 7, 8, 1, 9, 5, 3, 4, 2],
+          [9, 1, 2, 3, 4, 8, 5, 6, 7],
+          [8, 5, 9, 4, 2, 6, 7, 1, 3],
+          [7, 6, 1, 8, 5, 3, 9, 2, 4],
+          [4, 2, 3, 7, 9, 1, 8, 5, 6],
+          [9, 6, 1, 2, 8, 7, 3, 4, 5],
+          [5, 3, 7, 4, 1, 9, 2, 8, 6],
+          [2, 8, 4, 6, 3, 5, 1, 7, 9]
+        ]
+      );
+    });
   });
 });
