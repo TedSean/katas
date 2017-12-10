6af504a896556ab7377b48364a48c7ce95a23eb7
Arnoldas Sescila <arnoldass@wix.com>
2017-12-11 00:34:11 +0200

    10 Second part

--- test/knot-hash.js
+++ test/knot-hash.js
@@ -1,50 +1,104 @@
 let expect = require("chai").expect;
+let _ = require("lodash");
 
-const numbers = new Array(256).fill(null).map((val, index) => index);
-const input = `206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3`;
+const knotHash = (numbers, input, initialIndex, initialSkipSize) => {
+  return input.reduce((data, length) => {
+    const {index, skipSize, numbers} = data;
+    const reversedArray = getReversedArray(numbers, length, index);
 
-const demoNumbers = [0, 1, 2, 3, 4];
-const demoInput = `3,4,1,5`;
-
-const knotHash = (numbers, input) => {
-  const lengths = input.split(',').map(x => Number(x));
-  const a = lengths.reduce((data, length, skipSize) => {
-    const {index} = data;
-    const reversedArray = getReversedArray(data.numbers, length, index);
-
-    const y = fillWithMissingAfterReverse(data.numbers, reversedArray, index);
-    return {index: getArrayIndex(data.numbers, index + length + skipSize), numbers: y};
-  }, {index: 0, numbers});
-  return a.numbers[0] * a.numbers[1];
+    const y = fillWithMissingAfterReverse(numbers, reversedArray, index);
+    return {index: getArrayIndex(numbers.length, index + length + skipSize), numbers: y, skipSize: skipSize + 1};
+  }, {index: initialIndex, numbers, skipSize: initialSkipSize});
 }
 
 const fillWithMissingAfterReverse = (origArray, reversedValues, index) => {
   const arr = new Array(origArray.length);
   for (let i = 0; i < origArray.length; i++) {
-    arr[getArrayIndex(arr, index + i)] = (reversedValues[i] || reversedValues[i] === 0) ? reversedValues[i] : origArray[getArrayIndex(arr, index + i)]
+    arr[getArrayIndex(arr.length, index + i)] = (reversedValues[i] || reversedValues[i] === 0) ? reversedValues[i] : origArray[getArrayIndex(arr.length, index + i)]
   }
   return arr;
 }
 
-const getArrayIndex = (array, index) => {
-  return index < array.length ? index : index - array.length;
+const getArrayIndex = (length, index) => {
+  return index % length;
 }
 
 const getReversedArray = (origArray, length, start) => {
   const arr = [];
   for (let i = 0; i < length; i++) {
-    arr.push(origArray[getArrayIndex(origArray, start + i)]);
+    arr.push(origArray[getArrayIndex(origArray.length, start + i)]);
   }
   return arr.reverse();
 }
 
+const parsePart1Input = (input) =>
+  input.split(',')
+    .map(x => Number(x));
+
+const getPart1Answer = (input) =>
+  input.numbers[0] * input.numbers[1]
+
+const parsePart2Input = (input) =>
+  input.split('')
+    .map(x =>
+      x.charCodeAt(0))
+    .concat([17, 31, 73, 47, 23]);
+
+const getDenseHash = (sparseHash) =>
+  _.chunk(sparseHash, 16)
+    .map(chunk =>
+      chunk
+        .reduce((a, b) =>
+          a ^ b, 0));
+
+const getPart2Answer = (numbers, input) => {
+  let data = {index: 0, skipSize: 0, numbers}
+  for (let i = 0; i < 64; i++) {
+    data = knotHash(data.numbers, input, data.index, data.skipSize)
+  }
+
+  return encode(getDenseHash(data.numbers));
+}
+
+const encode = denseHash =>
+  denseHash
+    .map(a => a.toString(16))
+    .map(x => {
+      console.log(x.length === 1 ? `0${x}` : x);
+      return x.length === 1 ? `0${x}` : x
+    })
+    .join('')
+
 describe('Knot Hash', () => {
   it('should solve ', () => {
-    expect(knotHash(demoNumbers, demoInput)).to.equal(12);
+    const numbers = [0, 1, 2, 3, 4];
+    const input = `3,4,1,5`;
+    expect(getPart1Answer(knotHash(numbers, parsePart1Input(input), 0, 0))).to.equal(12);
   });
 
   it('should solve ', () => {
-    expect(knotHash(numbers, input)).to.equal(9656);
+    const numbers = new Array(256).fill(null).map((val, index) => index);
+    const input = `206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3`;
+    expect(getPart1Answer(knotHash(numbers, parsePart1Input(input), 0, 0))).to.equal(9656);
+  });
+
+  describe('second part', () => {
+    it('should parse input', () => {
+      expect(parsePart2Input('1,2,3').join(',')).to.equal('49,44,50,44,51,17,31,73,47,23')
+    });
+
+    it('should encode correctly', () => {
+      expect(encode([64, 7, 255])).to.equal('4007ff')
+    });
+
+    it('should work with examples', () => {
+      const numbers = new Array(256).fill(null).map((val, index) => index);
+      expect(getPart2Answer(numbers, parsePart2Input(''))).to.equal('a2582a3a0e66e6e86e3812dcb672a272')
+      expect(getPart2Answer(numbers, parsePart2Input(`1,2,4`))).to.equal('63960835bcdc130f0b66d7ff4f6a5a8e')
+      expect(getPart2Answer(numbers, parsePart2Input(`206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3`))).to.equal('20b7b54c92bf73cf3e5631458a715149')
+    });
   });
 });
+
+
 
